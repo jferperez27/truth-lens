49a0e6948c7ff233bfa4ba3d9ae61746476922ba
jferperez27 <juan_j_fernandez@brown.edu>
2025-06-17 22:50:30 -0500

    sample bias addition

---
+++ bias_hash.py
@@ -0,0 +1,17 @@
+BIAS_MAP = {
+    "cnn.com": "Left",
+    "nytimes.com": "Left-Center",
+    "msnbc.com": "Left",
+    "npr.org": "Left-Center",
+    "bbc.com": "Center",
+    "reuters.com": "Center",
+    "apnews.com": "Center",
+    "wsj.com": "Center-Right",
+    "foxnews.com": "Right",
+    "breitbart.com": "Far-Right",
+    "dailywire.com": "Right",
+    "theguardian.com": "Left",
+    "huffpost.com": "Left",
+    "newsmax.com": "Right",
+    "oann.com": "Far-Right"
+}
